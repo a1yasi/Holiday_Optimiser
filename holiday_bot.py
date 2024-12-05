41e0ddfd35ad4d32fc9b15bf5455047de5451392
a1yasi <alyasi.alyasi@hogarth.com>
2024-12-05 11:47:35 +0000

    fetching data

--- holiday_bot.py
+++ holiday_bot.py
@@ -4,8 +4,16 @@ import requests
 import json
 from datetime import datetime, timedelta
 
+client = AzureOpenAI(
+	api_key = os.getenv("AZURE_KEY"),
+	api_version = "2023-10-01-preview",
+	azure_endpoint = os.getenv("AZURE_ENDPOINT")
+)
 
-
-
+def get_holidays(year,country_code):
+	url = f"https://date.nager.at/api/v3/PublicHolidays/{year}/{country_code}"
+	response = requests.get(url)
+	data = response.json()
+	return data
 
 
